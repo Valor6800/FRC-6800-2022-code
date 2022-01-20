b53874b6dd0ff17ad986f2137555f1a176710d2e
Amaan Syed <amaanpsyed@gmail.com>
2022-01-20 12:21:42 -0600

    added 5 ball auto

--- Competition/src/main/cpp/ValorAuto.cpp
+++ Competition/src/main/cpp/ValorAuto.cpp
@@ -33,7 +33,7 @@ frc::Pose2d daffy = frc::Pose2d(2.0_m,.057_m, frc::Rotation2d(0_deg));
 frc::Pose2d porky = frc::Pose2d(6_m, 0.19_m, frc::Rotation2d(20_deg));
 frc::Pose2d shoot = frc::Pose2d(2.626_m,0_m, frc::Rotation2d(-90_deg));
 
-auto moveBugs = frc::TrajectoryGenerator::GenerateTrajectory(
+    auto moveBugs = frc::TrajectoryGenerator::GenerateTrajectory(
         frc::Pose2d(0_m, 0_m, frc::Rotation2d(0_deg)),
         {frc::Translation2d{0_m,1.234_m}},
         bugs,
@@ -44,12 +44,17 @@ auto moveBugs = frc::TrajectoryGenerator::GenerateTrajectory(
         {frc::Translation2d{2.0_m, 0.057_m}},
         porky,
         config);
+    auto movePorkyFromDaffy = frc::TrajectoryGenerator::GenerateTrajectory(
+        daffy,
+        {},
+        porky,
+        config);
 
     auto moveDaffy = frc::TrajectoryGenerator::GenerateTrajectory(
-        frc::Pose2d(0_m, -2.516_m, frc::Rotation2d(-122.24_deg)),
-        {frc::Translation2d{-.2_m, -4_m}},
-        frc::Pose2d(.2_m, -6_m, frc::Rotation2d(-65_deg)),
-        reverseConfig);
+        bugs,
+        {},
+        daffy,
+        config);
 
     auto moveShoot = frc::TrajectoryGenerator::GenerateTrajectory(
         porky,
@@ -67,7 +72,7 @@ auto moveBugs = frc::TrajectoryGenerator::GenerateTrajectory(
         [this] (auto states) { drivetrain->setModuleStates(states); },
         {drivetrain}
     );
-/*
+
     frc2::SwerveControllerCommand<4> cmd_move_moveDaffy(
         moveDaffy,
         [&] () { return drivetrain->getPose_m(); },
@@ -78,10 +83,20 @@ auto moveBugs = frc::TrajectoryGenerator::GenerateTrajectory(
         [this] (auto states) { drivetrain->setModuleStates(states); },
         {drivetrain}
     );
-    */
+    
 
     frc2::SwerveControllerCommand<4> cmd_move_movePorky(
         movePorky,
+        [&] () { return drivetrain->getPose_m(); },
+        drivetrain->getKinematics(),
+        frc2::PIDController(DriveConstants::KPX, DriveConstants::KIX, DriveConstants::KDX),
+        frc2::PIDController(DriveConstants::KPY, DriveConstants::KIY, DriveConstants::KDY),
+        thetaController,
+        [this] (auto states) { drivetrain->setModuleStates(states); },
+        {drivetrain}
+    );
+    frc2::SwerveControllerCommand<4> cmd_move_movePorkyFromDaffy(
+        movePorkyFromDaffy,
         [&] () { return drivetrain->getPose_m(); },
         drivetrain->getKinematics(),
         frc2::PIDController(DriveConstants::KPX, DriveConstants::KIX, DriveConstants::KDX),
@@ -114,6 +129,26 @@ auto moveBugs = frc::TrajectoryGenerator::GenerateTrajectory(
     (cmd_move_moveBugs,
     frc2::WaitCommand((units::second_t)1.5),
     cmd_move_movePorky,
+    cmd_move_moveShoot);
+
+    frc2::SequentialCommandGroup *shoot5 = new frc2::SequentialCommandGroup();
+    shoot5->AddCommands
+    (cmd_move_moveBugs,
+    frc2::WaitCommand((units::second_t)1.5),
+    cmd_move_moveDaffy,
+    frc2::WaitCommand((units::second_t)1.5),
+    cmd_move_movePorkyFromDaffy,
+    frc2::WaitCommand((units::second_t)1.5),
+    cmd_move_moveShoot);  
+
+    frc2::SequentialCommandGroup *shoot5RemoveTaz = new frc2::SequentialCommandGroup();
+    shoot5RemoveTaz->AddCommands
+    (cmd_move_moveBugs,
+    frc2::WaitCommand((units::second_t)1.5),
+    cmd_move_moveDaffy,
+    frc2::WaitCommand((units::second_t)1.5),
+    cmd_move_movePorkyFromDaffy,
+    frc2::WaitCommand((units::second_t)1.5),
     cmd_move_moveShoot); 
 /*
     frc2::SequentialCommandGroup *leaveTarmac = new frc2::SequentialCommandGroup();
